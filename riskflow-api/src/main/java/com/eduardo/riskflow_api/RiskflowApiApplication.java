60b6f560122da6eaaf977e74136c2643aec1de44
Eduardo Zaniboni <eduardowz.prog@gmail.com>
2026-08-07 21:22:20 -0300

    docs: inicializa projetos base e configs do docker para postgres e redis

---
+++ riskflow-api/src/main/java/com/eduardo/riskflow_api/RiskflowApiApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo.riskflow_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskflowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiskflowApiApplication.class, args);
+	}
+
+}
---
+++ riskflow-api/src/test/java/com/eduardo/riskflow_api/RiskflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.riskflow_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskflowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
